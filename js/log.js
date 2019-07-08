b6f0e1718fa1ef05aa9db06b8a75851e01e08248
Jinqi Li <jxl2047@miami.edu>
2019-07-08 12:32:14 -0400

    Ver1

---
+++ js/log.js
@@ -0,0 +1,21 @@
+// var form = document.querySelector("form");
+// var log = document.querySelector("#log");
+//
+// form.addEventListener("submit", function(event) {
+//   var data = new FormData(form);
+//   var output = "";
+//   for (const entry of data) {
+//     output = entry[0] + "=" + entry[1] + "\r";
+//   };
+//   log.innerText = output;
+//   event.preventDefault();
+// }, false);
+//
+// var rates = document.getElementsByClassName('Q1');
+// var radio_value;
+// for(var i = 0; i < rates.length; i++){
+//     if(rates[i].checked){
+//         radio_value = rates[i].value;
+//             console.log(radio_value);
+//     }
+// }
